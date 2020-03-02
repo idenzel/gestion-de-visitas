b2051661f20ae736bf79639fd97d8e6028d8293a
idenzel <iericor@gmail.com>
2020-03-02 15:05:00 -0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>Bonitos | Gestión de visitantes 2020.</p>
+                                </div>
+                            </div>
+                        </div>
---
+++ includes/sidebar.php
@@ -0,0 +1,32 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+        
+                   <h1>Bonitos visitas</h1>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: blue">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+
+     <li>
+                            <a href="visitors-form.php"  style="color: blue">
+                                <i class="fa fa-user"></i>Nuevo visitante</a>
+                        </li>
+   <li>
+                            <a href="manage-newvisitors.php"  style="color: blue">
+                                <i class="fa fa-users"></i>Gestor de visitas</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: blue">
+                                <i class="fas fa-copy"></i>Reporte de visitantes entre fechas</a>
+                        </li>  
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
